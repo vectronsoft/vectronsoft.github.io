6a6e9594fb63f4e545703c5f359f6ac382b1cb15
zeroflag <amagyar@hortonworks.com>
2021-05-29 00:54:38 +0200

    x

--- ynm/enc.sh
+++ ynm/enc.sh
@@ -2,5 +2,5 @@
 
 rm *.qst
 rm *.bin
-openssl enc -nosalt -des -in questions1.json -out questions1.bin -K $1 -iv 0
+openssl enc -nosalt -des -in questions1.json -out questions1.bin  -iv 0
 base64 -i questions1.bin -o questions1.qst
